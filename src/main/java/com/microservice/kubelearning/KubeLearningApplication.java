3d459efde221c80ada23bb8e89ece006c66dfb54
siddharthlucky <siddharth.luckyreddy@gmail.com>
2024-11-01 14:50:54 +0530

    - Initial commit

---
+++ src/main/java/com/microservice/kubelearning/KubeLearningApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.kubelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubeLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/kubelearning/KubeLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.kubelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
